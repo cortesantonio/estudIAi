c32393a27af6e9f2dc500d77475ad264ae5e9f05
cortesantonio <antonio.cortes.sotelo@gmail.com>
2025-07-12 23:35:38 -0400

    feat: add interfaces for Quiz, Question, Option, Answer, Session, and SessionAnswered

---
+++ src/interfaces/Quizzes.ts
@@ -0,0 +1,53 @@
+import type { Group } from "./Group";
+
+export interface Session {
+    id: number;
+    studyGroupId: number;
+    createdAt: Date;
+    quizId?: number;
+    studyGroup: Group;
+    sessionAnswered: SessionAnswered[];
+    quiz?: Quiz;
+}
+
+export interface SessionAnswered {
+    id: number;
+    sessionId: number;
+    userId: number;
+    answeredAt: Date;
+    score: number;
+}
+
+export interface Quiz {
+    id: number;
+    title: string;
+    description: string;
+    duration?: number; // en minutos
+    createdAt?: Date;
+    quatityQuestion?: number;
+    groupId?: number;
+    group?: Group;
+    questions?: Question[];
+}
+
+export interface Question {
+    id: number;
+    quizId: number;
+    text: string;
+    options: Option[];
+}
+
+export interface Option {
+    id: number;
+    questionId: number;
+    text: string;
+    isCorrect: boolean;
+}
+
+export interface Answer {
+    id: number;
+    userId: number;
+    optionId: number;
+    questionId: number;
+    answeredAt: Date;
+}
