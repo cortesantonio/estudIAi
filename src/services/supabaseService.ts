a71008e0d3eb2d516568a375ce2c8268130056d1
cortesantonio <antonio.cortes.sotelo@gmail.com>
2025-07-29 21:26:30 -0400

    feat: migracion de servidor

--- src/services/supabaseService.ts
+++ src/services/supabaseService.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xltjvdimmmqcnmbpqqie.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhsdGp2ZGltbW1xY25tYnBxcWllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE0MTEyOTgsImV4cCI6MjA2Njk4NzI5OH0.feUPxRXvg_38-kWNFX17XsMQcNNv5aXZDOApYj1I9zE';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
